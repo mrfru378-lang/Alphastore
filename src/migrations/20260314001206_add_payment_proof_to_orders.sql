bd1450439f89389b31b5aa555741ee76b730c98a
mrfru378-lang <mrfru378@gmail.com>
2026-03-17 00:01:27 +0600

    Delete src/migrations directory

--- src/migrations/20260314001206_add_payment_proof_to_orders.sql
+++
@@ -1,5 +0,0 @@
--- Add payment_proof_url column to orders table
-ALTER TABLE public.orders ADD COLUMN payment_proof_url TEXT;
-
--- Add comment for documentation
-COMMENT ON COLUMN public.orders.payment_proof_url IS 'URL to uploaded payment proof image for bKash/Nagad transactions';
